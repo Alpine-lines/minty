68ddd827078e6845ffb142c7db0bec0676444361
Alpine-lines <healey.josh@yahoo.com>
2022-03-04 13:39:57 -0700

    Updated PreMinty.sol and Pre721.sol

--- contracts/PreMinty.sol
+++ contracts/PreMinty.sol
@@ -12,10 +12,12 @@ contract PreMinty is Pre721 {
     constructor(
         string memory _name, 
         string memory _symbol, 
+        address[] _minters,
+        address[] _admins,
         string memory _contractURI, 
         uint256 _maxSupply,
         address _proxyRegistryAddress
     )
-        Pre721(_name, _symbol, _contractURI,_maxSupply, _proxyRegistryAddress)
+        Pre721(_name, _symbol,_minters, _admins, _contractURI,_maxSupply, _proxyRegistryAddress)
     {}
 }
